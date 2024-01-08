37a47df6a7e45dab9f94158cc1a578e9856a2d4d
Gaspare <Gas9220@users.noreply.github.com>
2024-01-08 11:31:27 +0100

    Initial Commit

---
+++ FaceFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceFacts
+//
+//  Created by Gaspare Monte on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Gaspare Monte on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
