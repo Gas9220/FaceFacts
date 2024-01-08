97bfe3714793e6699709481dd2561a2c3992e62e
Gaspare <Gas9220@users.noreply.github.com>
2024-01-08 15:07:18 +0100

    feat: add photo property to person

--- FaceFacts/EditPersonView.swift
+++ FaceFacts/EditPersonView.swift
@@ -7,11 +7,14 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct EditPersonView: View {
     @Environment(\.modelContext) var modelContext
     @Binding var navigationPath: NavigationPath
     @Bindable var person: Person
+    @State private var selectedItem: PhotosPickerItem?
+
     @Query(sort: [
         SortDescriptor(\Event.name),
         SortDescriptor(\Event.location)
@@ -19,6 +22,21 @@ struct EditPersonView: View {
 
     var body: some View {
         Form {
+            Section {
+                if let imageData = person.photo,
+                   let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxHeight: 300)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    Label(person.photo == nil ? "Select a photo" : "Change photo", systemImage: "person")
+                }
+            }
+
             Section {
                 TextField("Name", text: $person.name)
                     .textContentType(.name)
@@ -55,12 +73,19 @@ struct EditPersonView: View {
         .navigationDestination(for: Event.self) { event in
             EditEventView(event: event)
         }
+        .onChange(of: selectedItem, loadPhoto)
     }
 
     func addEvent() {
         let event = Event(name: "", location: "")
         modelContext.insert(event)
         navigationPath.append(event)
+    }
+
+    func loadPhoto() {
+        Task { @MainActor in
+            person.photo = try await selectedItem?.loadTransferable(type: Data.self)
+        }
     }
 }
 
