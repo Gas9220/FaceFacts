9fa846dfa8be07291ccd648361bda6c8034f887d
Gaspare <Gas9220@users.noreply.github.com>
2024-01-08 12:46:14 +0100

    feat: add Event model

---
+++ FaceFacts/Event.swift
@@ -0,0 +1,21 @@
+//
+//  Event.swift
+//  FaceFacts
+//
+//  Created by Gaspare Monte on 08/01/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Event {
+    var name: String
+    var location: String
+    var people = [Person]()
+    
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
+}
